a1c2792f83526e4ae5052757d3774755b8fe1d2d
Agoston Szepessy <agszepp@gmail.com>
2016-12-07 12:30:54 -0500

    Fixed bug where OpenGL functions weren't loaded before their use

--- src/main.rs
+++ src/main.rs
@@ -9,16 +9,15 @@ use graphics::Model;
 fn main() {
     let window = glutin::Window::new().unwrap();
 
-    let model = Model::new("bear.obj");
-
     unsafe { window.make_current() };
 
     unsafe {
-        // TODO: fix this because it doesn't work
         gl::load_with(|symbol| window.get_proc_address(symbol) as *const _);
 
         gl::ClearColor(0.0, 1.0, 0.0, 1.0);
     }
+
+    let model = Model::new("bear.obj");
 
     for event in window.wait_events() {
         unsafe { gl::Clear(gl::COLOR_BUFFER_BIT) };
